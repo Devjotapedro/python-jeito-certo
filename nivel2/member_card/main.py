05dd4206c13c6ae524b6f39160074b2550c184d1
João Pedro <dev.jotapedro@gmail.com>
2026-04-21 14:58:24 -0300

    feat: concluido background

--- nivel2/member_card/main.py
+++ nivel2/member_card/main.py
@@ -1,8 +1,31 @@
-from reportlab.graphics.shapes import Drawing
+from reportlab.lib import colors
+from reportlab.graphics.shapes import (Drawing,
+Rect)
 OUT_DIR = "grafica"
 WIDTH = 640
 HEIGHT = 360
 
-drawing = Drawing(WIDTH, HEIGHT)
-# save
-drawing.save(formats=['pdf'], outDir=OUT_DIR, fnRoot="carteirinha")
+PG_COLORS = {
+    "blue": colors.HexColor("#062846"),
+    "green": colors.HexColor("#109395") 
+}
+
+def draw_background(drawing, margin=(16, 16), radius=32):
+    #00 é a primeira coordenada
+    #reetangulo externo
+    outer_rect = Rect(0,0, WIDTH, HEIGHT, fillColor = PG_COLORS["green"])
+    drawing.add(outer_rect)
+    
+    xmargin = margin[0]
+    ymargin = margin[1]
+    #retangulo interno
+    inner_rect = Rect(xmargin,ymargin, WIDTH - 2 * xmargin, HEIGHT - 2 * ymargin, radius, radius, fillColor = PG_COLORS["blue"], strokeColor = PG_COLORS["blue"])
+    drawing.add(inner_rect)
+
+if __name__ == '__main__':
+
+    drawing = Drawing(WIDTH, HEIGHT)
+    #Desenha o fundo
+    draw_background(drawing)
+    # save
+    drawing.save(formats=['pdf'], outDir=OUT_DIR, fnRoot="carteirinha")
